a3e4e9ae14822e1bfabf854eebb66d74bb65c265
m0oneycomp <gallostarshine@gmail.com>
2026-03-06 22:19:14 +1100

    yo

---
+++ loader.lua
@@ -0,0 +1,13 @@
+-- loader.lua
+-- Simple GitHub-ready Lua script
+
+print("Loader started")
+
+local message = "Script loaded successfully from GitHub!"
+print(message)
+
+local function hello()
+    print("Hello from the GitHub script!")
+end
+
+hello()
